1e28ab3ddf2e25a2fa0fd71e46c1f2a2ff48d692
RaulGarvDev <raulgarvdev@gmail.com>
2022-02-08 20:26:26 +0100

    Diseño de pantallas iniciales al 100%

--- LoopBook/Controladores/LoginViewController.swift
+++ LoopBook/Controladores/LoginViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var email_TextField: UITextField!
+    @IBOutlet weak var contraseña_TextField: UITextField!
+    @IBOutlet weak var olvidoContraseña_Boton: UIButton!
+    @IBOutlet weak var iniciarSesion_Boton: UIButton!
+    @IBOutlet weak var facebook_login: UIImageView!
+    @IBOutlet weak var gmail_Login: NSLayoutConstraint!
+    @IBOutlet weak var registro_Boton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

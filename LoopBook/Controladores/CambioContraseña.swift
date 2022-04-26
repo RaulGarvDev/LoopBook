e2376e390be950c7dec9ec9111220e48490691d0
RaulGarvDev <raulgarvdev@gmail.com>
2022-04-26 20:06:21 +0200

    Cambio de contraseña implementado

--- LoopBook/Controladores/CambioContraseña.swift
+++ LoopBook/Controladores/CambioContraseña.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class CambioContraseña: UIViewController {
     
@@ -23,6 +25,35 @@ class CambioContraseña: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cambiarContraseña(_ sender: Any) {
+        
+        let auth = Auth.auth()
+        
+        auth.sendPasswordReset(withEmail: email_TextField.text!) { (error) in
+           
+            if error == nil{
+                
+                let alert = UIAlertController(title: "Email enviado",
+                                              message: "Revisa tu correo",
+                                              preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true)
+                
+            }else{
+                
+                let alert = UIAlertController(title: "Error",
+                                              message: "Comprueba que el email es correcto",
+                                              preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true)
+            }
+                
+            
+        }
+    }
+    
 }
 
